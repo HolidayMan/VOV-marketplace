b121692915c9d34f0fca30a5e628a68a9426cf32
Alex Pitenko <oleksii.p@itsyndicate.org>
2023-05-25 21:26:09 +0200

    added media/ route

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ from fastapi import FastAPI
 from starlette.staticfiles import StaticFiles
 
 from routers import catalog_controller
-from settings import STATIC_ROOT, STATIC_URL
+from settings import STATIC_ROOT, STATIC_URL, MEDIA_ROOT, MEDIA_URL
 
 
 app = FastAPI()
 app.include_router(catalog_controller.router)
 app.mount(STATIC_URL, StaticFiles(directory=STATIC_ROOT), name="static")
+app.mount(MEDIA_URL, StaticFiles(directory=MEDIA_ROOT), name="media")
 
 
 @app.get("/")
