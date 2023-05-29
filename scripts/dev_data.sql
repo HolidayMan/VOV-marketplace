6d605cc36a4e054c94788b4537da806b3b279c61
Alex Pitenko <oleksii.p@itsyndicate.org>
2023-05-29 23:43:51 +0200

    added generating data scripts to docker-compose.yml

---
+++ scripts/dev_data.sql
@@ -0,0 +1,3 @@
+# Adding users
+INSERT INTO vov_database.user (id, name, email, password, role_id) VALUES (1, 'Vlad', 'vlad@gmail.com', '$2b$12$3DMcWqyvXTd2GUnJLa79Le379rKv2AKXfC7IOCYCOHMyEvK5WUwWu', 1);
+INSERT INTO vov_database.user (id, name, email, password, role_id) VALUES (2, 'Oleksiy', 'alex@gmail.com', '$2b$12$zhCEhiljh1EnCGyyK5ZO9eEMBR1D8Zs/MMeF3XwcG/NyElg9Y21ru', 2);
